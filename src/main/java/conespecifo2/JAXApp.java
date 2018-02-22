7cf9941fd5aa5e98a1281269deff42192343e164
Andres <andres@localhost.localdomain>
2018-02-22 20:49:43 -0300

    first commit

---
+++ src/main/java/conespecifo2/JAXApp.java
@@ -0,0 +1,9 @@
+package conespecifo2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("pipoc")
+public class JAXApp extends Application {
+
+}
---
+++ src/main/java/conespecifo2/endpoint.java
@@ -0,0 +1,16 @@
+package conespecifo2;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("endpoint")
+public class endpoint {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getDataJSON() {
+		return "It works the conector!";
+	}
+}
